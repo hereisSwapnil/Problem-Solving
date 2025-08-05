dad13f2f96243ef3f67254669fc5f678500a92da
Swapnil Singh <85278243+hereisSwapnil@users.noreply.github.com>
2025-08-06 01:50:07 +0530

    Time: 2 ms (32.91%) | Memory: 28.2 MB (27.82%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -1,7 +1,18 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        return nums[nums.size()/2];
+        int can = nums[0];
+        int c = 0;
+        for (int n: nums){
+            if(c == 0){
+                can = n;
+            }
+            if(can == n){
+                c++;
+            }else{
+                c--;
+            }
+        }
+        return can;
     }
 };
