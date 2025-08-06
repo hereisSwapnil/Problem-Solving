c22e30d14419819ff9661edcb59df4cc2003c4bb
Swapnil Singh <85278243+hereisSwapnil@users.noreply.github.com>
2025-08-06 16:16:04 +0530

    Time: 0 ms (100.00%) | Memory: 15.8 MB (32.95%) - LeetSync

--- 31-next-permutation/next-permutation.cpp
+++ 31-next-permutation/next-permutation.cpp
@@ -1,9 +1,21 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        next_permutation(nums.begin(), nums.end());
+        // next_permutation(nums.begin(), nums.end());
         // Now implementing the STL part of this
-
         
+        int n = nums.size();
+        int i = n-2;
+        while(i>=0 && nums[i]>=nums[i+1]){
+            i--;
+        }
+        if(i>=0){
+            int j = n-1;
+            while(nums[i]>=nums[j]){
+                j--;
+            }
+            swap(nums[i], nums[j]);
+        }
+        reverse(nums.begin()+i+1, nums.end());
     }
 };
