d1ca94099fc77e52ec531af00b2c9e05e425567f
Swapnil Singh <85278243+hereisSwapnil@users.noreply.github.com>
2025-08-08 19:41:44 +0530

    Time: 80 ms (31.51%) | Memory: 54 MB (7.96%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -2,17 +2,33 @@ class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
         // Bruteforce Approach
+        // int sum = 0;
+        // int count = 0;
+        // int n = nums.size();
+        // for(int i = 0; i<n; i++){
+        //     sum = 0;
+        //     for(int j = i; j<n; j++){
+        //         sum += nums[j];
+        //         if(sum == k){
+        //             count++;
+        //         }
+        //     }
+        // }
+        // return count;
+
+
+        // Optimised Approach
         int sum = 0;
-        int count = 0;
         int n = nums.size();
+        int preSum = 0;
+        int count = 0;
+        map<int, int> mp;
+        mp[0] = 1;
         for(int i = 0; i<n; i++){
-            sum = 0;
-            for(int j = i; j<n; j++){
-                sum += nums[j];
-                if(sum == k){
-                    count++;
-                }
-            }
+            preSum += nums[i];
+            int remove = preSum - k;
+            count += mp[remove];
+            mp[preSum] += 1;
         }
         return count;
     }
