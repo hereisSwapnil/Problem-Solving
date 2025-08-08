7a9121c26232b75e242c1a37d955d49b411bd19b
Swapnil Singh <85278243+hereisSwapnil@users.noreply.github.com>
2025-08-08 20:46:21 +0530

    Time: 0 ms (100.00%) | Memory: 9.5 MB (89.24%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.cpp
+++ 118-pascals-triangle/pascals-triangle.cpp
@@ -11,11 +11,30 @@ public:
     }
     vector<vector<int>> generate(int numRows) {
         // bruteforce
+        // vector<vector<int>> res;
+        // for(int n = 1; n<=numRows; n++){
+        //     vector<int> v(n);
+        //     for(int r = 1; r<=n; r++){
+        //         v[r-1] = nCr(n-1, r-1);
+        //     }
+        //     res.push_back(v);
+        // }
+        // return res;
+
+        // Optimised Version
+
         vector<vector<int>> res;
-        for(int n = 1; n<=numRows; n++){
-            vector<int> v(n);
+        for(int n = 0; n<numRows; n++){
+            vector<int> v(n+1);
+            v[0] = 1;
+            int val = 1;
+            int num = n;
+            int den = 1;
             for(int r = 1; r<=n; r++){
-                v[r-1] = nCr(n-1, r-1);
+                val = (val * num) / den;
+                v[r] = val;
+                num--;
+                den++;
             }
             res.push_back(v);
         }
