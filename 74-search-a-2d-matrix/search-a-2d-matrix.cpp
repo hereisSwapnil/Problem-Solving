934999aa3b9873f18c069f688c854afd411589cc
Swapnil Singh <85278243+hereisSwapnil@users.noreply.github.com>
2025-08-11 18:45:27 +0530

    Time: 1 ms (2.62%) | Memory: 13.4 MB (7.28%) - LeetSync

---
+++ 74-search-a-2d-matrix/search-a-2d-matrix.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int n = matrix.size();
+        int gap = matrix[0].size();
+        int i = 0;
+        bool found = false;
+        while(!found){
+            if(matrix[i][0] <= target && target <= matrix[i][gap-1]){
+                found = true;
+                cout << "found" << endl;
+                break;
+            }
+            cout << matrix[i][0] << "done" << matrix[i][gap-1] << endl;
+            i++;
+            if(i>=n){
+                return false;
+            }
+        }
+        cout<<i<<endl;
+        vector<int> nums = matrix[i];
+        int left = 0;
+        int right = gap - 1;
+        while(left<=right){
+            int mid = left + (right-left)/2;
+            if(nums[mid] == target){
+                return true;
+            }
+            if(nums[mid]>=target){
+                right = mid - 1;
+            }else{
+                left = mid + 1;
+            }
+        }
+        return false;
+    }
+};
