b652cdfeb8cc17122e9798a881c1a3bfb5010d23
Swapnil Singh <85278243+hereisSwapnil@users.noreply.github.com>
2025-08-11 17:33:00 +0530

    Time: 425 ms (0.85%) | Memory: 11.7 MB (48.60%) - LeetSync

--- 75-sort-colors/sort-colors.cpp
+++ 75-sort-colors/sort-colors.cpp
@@ -1,6 +1,18 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
+        for(int i = 0; i<nums.size(); i++){
+            int j = 0;
+            while(j<nums.size()-1-i){
+                if(nums[j]>nums[j+1]){
+                    swap(nums[j], nums[j+1]);
+                }
+                j++;
+            }
+            for(int i: nums){
+                cout << i << " ";
+            }
+            cout << endl;
+        }
     }
 };
