4b6bb6b78d2dbd84c5dda74d776ed21ad86a222c
Swapnil Singh <85278243+hereisSwapnil@users.noreply.github.com>
2025-08-11 16:00:04 +0530

    Time: 0 ms (100.00%) | Memory: 18.6 MB (51.29%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -1,62 +1,35 @@
 class Solution {
 public:
-    void zeroRow(vector<vector<int>> &matrix, int row){
-        int m = matrix.size();
-        for (int i = 0; i < m; i++){
-            if(matrix[i][row] !=0 ){
-                matrix[i][row] = 0;
-            }
+    void zeroRow(vector<vector<int>>& matrix, int row){
+        for(int i = 0; i<matrix[0].size(); i++){
+            matrix[row][i] = 0;
         }
     }
-
-    void zeroCol(vector<vector<int>> &matrix, int col){
-        int n = matrix[0].size();
-        for (int i = 0; i < n; i++){
-            if(matrix[col][i] !=0 ){
-                matrix[col][i] = 0;
-            }
+    void zeroCol(vector<vector<int>>& matrix, int col){
+        for(int i = 0; i<matrix.size(); i++){
+            matrix[i][col] = 0;
         }
     }
-    
     void setZeroes(vector<vector<int>>& matrix) {
-        // BruteForce Sol.
-        // int m = matrix.size();
-        // int n = matrix[0].size();
-        // for (int i = 0; i < m; i++){
-        //     for (int j = 0; j < n; j++){
-        //         if(matrix[i][j] == 0){
-        //             zeroRow(matrix, j);
-        //             zeroCol(matrix, i);
-        //         }
-        //     }
-        // }
-        // for (int i = 0; i < m; i++){
-        //     for (int j = 0; j < n; j++){
-        //         if(matrix[i][j] == -1){
-        //             matrix[i][j] = 0;
-        //         }
-        //     }
-        // }
-    
-        // Better Solution
         vector<int> row;
         vector<int> col;
-
         int m = matrix.size();
         int n = matrix[0].size();
-        for (int i = 0; i < m; i++){
-            for (int j = 0; j < n; j++){
+
+        for(int i = 0; i<m; i++){
+            for(int j = 0; j<n; j++){
                 if(matrix[i][j] == 0){
-                    row.push_back(j);
-                    col.push_back(i);
+                    col.push_back(j);
+                    row.push_back(i);
                 }
             }
         }
-        for (int i = 0; i<row.size(); i++){
-            zeroRow(matrix, row[i]);
+
+        for(int i: row){
+            zeroRow(matrix, i);
         }
-        for (int i = 0; i<col.size(); i++){
-            zeroCol(matrix, col[i]);
+        for(int i: col){
+            zeroCol(matrix, i);
         }
     }
 };
