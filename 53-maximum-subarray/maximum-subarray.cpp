093e3940b08231fed7941411f05834ad8b6a07b8
Swapnil Singh <85278243+hereisSwapnil@users.noreply.github.com>
2025-08-11 16:46:11 +0530

    Time: 1 ms (26.79%) | Memory: 71.8 MB (51.89%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.cpp
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -4,9 +4,7 @@ public:
         int sum = nums[0];
         int maxSum = nums[0];
         for(int i = 1; i<nums.size(); i++){
-            if(sum>0){
-                maxSum = max(maxSum, sum);
-            }else{
+            if(sum < 0){
                 sum = 0;
             }
             sum += nums[i];
