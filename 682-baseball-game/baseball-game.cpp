20fdca6a2c690ff50b57595f6e36d7a4fb5c754e
Swapnil Singh <85278243+hereisSwapnil@users.noreply.github.com>
2025-08-19 22:58:18 +0530

    Time: 0 ms (100.00%) | Memory: 11.9 MB (80.42%) - LeetSync

---
+++ 682-baseball-game/baseball-game.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int calPoints(vector<string>& operations) {
+        vector<int> ans;
+        int sum = 0;
+        for(string i: operations){
+            if(i == "+"){
+                int n = ans.size();
+                sum += ans[n-1]+ans[n-2];
+                ans.push_back(ans[n-1]+ans[n-2]);
+            }
+            else if(i == "D"){
+                int n = ans.size();
+                sum += ans[n-1]*2;
+                ans.push_back(ans[n-1]*2);
+            }
+            else if(i == "C"){
+                int n = ans.size();
+                sum -= ans[n-1];
+                ans.pop_back();
+            }
+            else{
+                sum += stoi(i);
+                ans.push_back(stoi(i));
+            }
+        }
+        return sum;
+    }
+};
