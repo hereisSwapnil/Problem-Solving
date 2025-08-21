7a0cfc817467c40a7229e77786371029b9496043
Swapnil Singh <85278243+hereisSwapnil@users.noreply.github.com>
2025-08-22 03:40:22 +0530

    Time: 0 ms (100.00%) | Memory: 11 MB (40.37%) - LeetSync

--- 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.cpp
+++ 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.cpp
@@ -11,17 +11,39 @@
  */
 class Solution {
 public:
-    void dfs(TreeNode* root, vector<int>& result){
-        if(!root){
+    // Recursive Approach
+    // void dfs(TreeNode* root, vector<int>& result){
+    //     if(!root){
+    //         return;
+    //     }
+    //     result.push_back(root->val);
+    //     dfs(root->left, result);
+    //     dfs(root->right, result);
+    // }
+
+    // Iterative Approach
+    void iterativePre(TreeNode* root, vector<int>& result){
+        stack<TreeNode*> st;
+        if(root == NULL){
             return;
         }
-        result.push_back(root->val);
-        dfs(root->left, result);
-        dfs(root->right, result);
+        st.push(root);
+        while(!st.empty()){
+            TreeNode* node = st.top();
+            st.pop();
+            result.push_back(node->val);
+            if(node->right != NULL) st.push(node->right);
+            if(node->left != NULL) st.push(node->left);
+        }
+        return;
     }
+
     vector<int> preorderTraversal(TreeNode* root) {
         vector<int> result;
-        dfs(root, result);
+        // Recursive Approach
+        // dfs(root, result);
+        // Iterative Approach
+        iterativePre(root, result);
         return result;
     }
 };
