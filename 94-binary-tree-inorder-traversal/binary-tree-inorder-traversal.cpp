4c00b9abee811cdea5e89ed4195e21d981bdd732
Swapnil Singh <85278243+hereisSwapnil@users.noreply.github.com>
2025-08-23 00:23:26 +0530

    Time: 0 ms (100.00%) | Memory: 10.8 MB (86.72%) - LeetSync

---
+++ 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.cpp
@@ -0,0 +1,49 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    // recursive approach
+    // void dfs(TreeNode* root, vector<int>& result) {
+    //     if (!root) return;
+    //     dfs(root->left, result);
+    //     result.push_back(root->val);
+    //     dfs(root->right, result);
+    // }
+
+    // iterative approach
+    void interativeInorder(TreeNode* node, vector<int>& result){
+       stack<TreeNode*> st;
+       if(node == nullptr){
+            return;
+       }
+       while(!st.empty() || node != nullptr){
+            while(node != nullptr){
+                st.push(node);
+                node = node->left;
+            }
+            node = st.top();
+            st.pop();
+            result.push_back(node->val);
+            node = node->right; 
+       }
+    }
+
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> result;
+        // recursive approach
+        // dfs(root, result);
+
+        // Iterative approach
+        interativeInorder(root, result);
+        return result;
+    }
+};
