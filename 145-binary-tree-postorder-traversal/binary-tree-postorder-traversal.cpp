64e5d8d5d692ad9f598f73ff87f57107bfc18981
Swapnil Singh <85278243+hereisSwapnil@users.noreply.github.com>
2025-08-23 01:02:19 +0530

    Time: 0 ms (100.00%) | Memory: 10.9 MB (92.20%) - LeetSync

--- 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.cpp
+++ 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.cpp
@@ -11,16 +11,37 @@
  */
 class Solution {
 public:
-    void dfs(TreeNode* root, vector<int>& result) {
-        if (!root) return;
-        dfs(root->left, result);
-        dfs(root->right, result);
-        result.push_back(root->val);
+    // recursive solution
+    // void dfs(TreeNode* root, vector<int>& result) {
+    //     if (!root) return;
+    //     dfs(root->left, result);
+    //     dfs(root->right, result);
+    //     result.push_back(root->val);
+    // }
+
+    // 2 stack approach
+    void TwoPostOrder(TreeNode* root, vector<int>& result){
+        stack<TreeNode*> st1, st2;
+        if(root == nullptr) return;
+        st1.push(root);
+        while(!st1.empty()){
+            TreeNode* top = st1.top();
+            st1.pop();
+            st2.push(top);
+            if(top->left != nullptr) st1.push(top->left);
+            if(top->right != nullptr) st1.push(top->right);
+        }
+        while(!st2.empty()){
+            TreeNode* top = st2.top();
+            st2.pop();
+            result.push_back(top->val);
+        }
     }
 
     vector<int> postorderTraversal(TreeNode* root) {
         vector<int> result;
-        dfs(root, result);
+        // dfs(root, result);
+        TwoPostOrder(root, result);
         return result;
     }
 };
