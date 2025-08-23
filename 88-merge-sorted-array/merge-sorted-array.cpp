2e920c12ddfa6f11d5097fe5cf2536ddcf9807d1
Swapnil Singh <85278243+hereisSwapnil@users.noreply.github.com>
2025-08-23 13:18:06 +0530

    Time: 0 ms (100.00%) | Memory: 12.2 MB (95.69%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.cpp
+++ 88-merge-sorted-array/merge-sorted-array.cpp
@@ -1,33 +1,52 @@
 class Solution {
 public:
-    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        vector<int> res(m+n);
-        int i = 0;
-        int a = 0;
-        int b = 0;
-        while(a<m && b<n){
-            if(nums1[a]<nums2[b]){
-                res[i] = nums1[a];
-                a++;
-            }else{
-                res[i] = nums2[b];
-                b++;
-            }
-            i++;
-        }
-        while(a<m){
-            res[i] = nums1[a];
-            i++;
-            a++;
-        }
-        while(b<n){
-            res[i] = nums2[b];
-            i++;
-            b++;
-        }
+    // High Memory Solution
+    // void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+    //     vector<int> res(m+n);
+    //     int i = 0;
+    //     int a = 0;
+    //     int b = 0;
+    //     while(a<m && b<n){
+    //         if(nums1[a]<nums2[b]){
+    //             res[i] = nums1[a];
+    //             a++;
+    //         }else{
+    //             res[i] = nums2[b];
+    //             b++;
+    //         }
+    //         i++;
+    //     }
+    //     while(a<m){
+    //         res[i] = nums1[a];
+    //         i++;
+    //         a++;
+    //     }
+    //     while(b<n){
+    //         res[i] = nums2[b];
+    //         i++;
+    //         b++;
+    //     }
 
-        for(int i = 0; i<m+n; i++){
-            nums1[i] = res[i];
+    //     for(int i = 0; i<m+n; i++){
+    //         nums1[i] = res[i];
+    //     }
+    // }
+
+
+    // Implace Solution
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int a = m-1;
+        int b = n-1;
+        int k = m+n-1;
+        while(a>=0 && b>=0){
+            if(nums1[a]>nums2[b]){
+                nums1[k--] = nums1[a--];
+            }else{
+                nums1[k--] = nums2[b--];
+            }
+        }
+        while(b>=0){
+            nums1[k--] = nums2[b--];
         }
     }
 };
