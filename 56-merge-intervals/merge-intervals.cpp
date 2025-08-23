16894685756d7bc81898034d457485396a65d48b
Swapnil Singh <85278243+hereisSwapnil@users.noreply.github.com>
2025-08-23 14:53:49 +0530

    Time: 4 ms (69.34%) | Memory: 23.4 MB (97.90%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -1,21 +1,40 @@
 class Solution {
 public:
+    // Optimal solution
+    // vector<vector<int>> merge(vector<vector<int>>& intervals) {
+    //     sort(intervals.begin(), intervals.end(), [](const vector<int>& a, const vector<int>& b){
+    //         return a[0]<b[0];
+    //     });
+    //     vector<vector<int>> res;
+    //     int k = 0;
+    //     res.push_back(intervals[0]);
+    //     for(int i = 1; i<intervals.size(); i++){
+    //         vector<int>& last = res.back();
+    //         if(last[1] >= intervals[i][0]){
+    //             last[1] = max(intervals[i][1], last[1]);
+    //         }else{
+    //             res.push_back(intervals[i]);
+    //         }
+    //         k++;
+    //     }
+    //     return res;
+    // }
+
+    // Best for Space (In Place)
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
         sort(intervals.begin(), intervals.end(), [](const vector<int>& a, const vector<int>& b){
             return a[0]<b[0];
         });
-        vector<vector<int>> res;
         int k = 0;
-        res.push_back(intervals[0]);
         for(int i = 1; i<intervals.size(); i++){
-            vector<int>& last = res.back();
-            if(last[1] >= intervals[i][0]){
-                last[1] = max(intervals[i][1], last[1]);
+            if(intervals[k][1] >= intervals[i][0]){
+                intervals[k][1] = max(intervals[i][1], intervals[k][1]);
             }else{
-                res.push_back(intervals[i]);
+                k++;
+                intervals[k] = intervals[i];
             }
-            k++;
         }
-        return res;
+        intervals.resize(k+1);
+        return intervals;
     }
 };
