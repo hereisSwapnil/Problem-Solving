4b7c8921e98c968c3310e89d118436584a80fa98
Swapnil Singh <85278243+hereisSwapnil@users.noreply.github.com>
2025-08-25 01:42:55 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (83.53%) - LeetSync

---
+++ 231-power-of-two/power-of-two.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    // simple approach
+    // bool isPowerOfTwo(int n) {
+    //     while(n%2 == 0 || n == 1){
+    //         if(n == 1 || n == 2){
+    //             return true;
+    //         }
+    //         n /= 2;
+    //     }
+    //     return false;
+    // }
+    // using bit manupulation
+    bool isPowerOfTwo(int n) {
+        if (n <= 0) return false;
+        return (n & (n - 1)) == 0;
+    }
+};
