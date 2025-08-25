d110b8049e006f19dff7a1885baf79bfd0efb0e0
Swapnil Singh <85278243+hereisSwapnil@users.noreply.github.com>
2025-08-25 13:19:44 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (70.64%) - LeetSync

--- 50-powx-n/powx-n.cpp
+++ 50-powx-n/powx-n.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
     double power(double x, long long n){
-        if (n == 0) return 1;
-        if (n < 0) return 1 / power(x, -n);
-        if (n % 2 == 0) return power(x * x, n / 2);
-        return x * power(x * x, n / 2);
+       if(n == 0) return 1;
+       if(n<0) return 1/power(x, -n);
+       if(n%2 == 0) return power(x*x, n/2);
+       return x * power(x*x, n/2);
     }
     double myPow(double x, int n) {
         return power(x, n);
