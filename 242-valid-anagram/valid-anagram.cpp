a2a3b6d659186614d5edaaf9fa4d936bb1149a1f
Swapnil Singh <85278243+hereisSwapnil@users.noreply.github.com>
2025-08-25 18:06:41 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (26.35%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,16 +1,39 @@
 class Solution {
 public:
     // using map
-    bool isAnagram(string s, string t) {
-        unordered_map<char, int> mp;
-        for(char a: s){
-            mp[a]++;
+    // bool isAnagram(string s, string t) {
+    //     unordered_map<char, int> mp;
+    //     for(char a: s){
+    //         mp[a]++;
+    //     }
+    //     for(char a: t){
+    //         mp[a]--;
+    //     }
+    //     for(auto a: mp){
+    //         if(a.second>0 || a.second<0) return false;
+    //     }
+    //     return true;
+    // }
+    // // using sorting
+    // bool isAnagram(string s, string t) {
+    //     sort(s.begin(), s.end());
+    //     sort(t.begin(), t.end());
+    //     return s == t;
+    // }
+
+    // using character array
+    bool isAnagram(string s, string t) {        
+        if (s.size() != t.size()) return false;
+        
+        vector<int> count(26, 0);
+        for (char x : s) {
+            count[x - 'a']++;
         }
-        for(char a: t){
-            mp[a]--;
+        for (char x : t) {
+            count[x - 'a']--;
         }
-        for(auto a: mp){
-            if(a.second>0 || a.second<0) return false;
+        for (int val : count) {
+            if (val != 0) return false;
         }
         return true;
     }
