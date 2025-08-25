0a70ecbc746cceda5c4931a98fc9926d39ebffe1
Swapnil Singh <85278243+hereisSwapnil@users.noreply.github.com>
2025-08-25 20:25:44 +0530

    Time: 0 ms (100.00%) | Memory: 11.8 MB (84.60%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.cpp
+++ 14-longest-common-prefix/longest-common-prefix.cpp
@@ -5,7 +5,7 @@ public:
         string prefix = strs[0];
         for(int j = 1; j<strs.size(); j++){
             int i = 0;
-            while(i<prefix.size() && i<strs[j].size() && strs[j][i] == prefix[i]){
+            while(i<prefix.size() && i<strs[j].size() && strs[j][i]==prefix[i]){
                 i++;
             }
             prefix = prefix.substr(0, i);
