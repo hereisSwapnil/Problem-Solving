841faf1737a4124e55cd980f8d204adc78fbdb3d
Swapnil Singh <85278243+hereisSwapnil@users.noreply.github.com>
2025-08-27 13:28:08 +0530

    Time: 53 ms (13.25%) | Memory: 75.1 MB (29.87%) - LeetSync

---
+++ 940-fruit-into-baskets/fruit-into-baskets.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    // Naive Approach
+    // int totalFruit(vector<int>& fruits){
+    //     int n = fruits.size();
+    //     int maxL = 0;
+    //     for (int i = 0; i < n; i++)
+    //     {
+    //         pair<int, int> p = {-1, -1};
+    //         int j;
+    //         for (j = i; j < n; j++)
+    //         {
+    //             if (p.first == -1) {
+    //                 p.first = fruits[j];
+    //             } else if (p.second == -1 && fruits[j] != p.first) {
+    //                 p.second = fruits[j];
+    //             }
+
+    //             if (fruits[j] != p.first && fruits[j] != p.second) break;
+    //         }
+    //         j--;
+    //         maxL = max(maxL, j - i + 1);
+    //     }
+    //     return maxL;
+    // }
+
+    // Optimized approach
+    int totalFruit(vector<int>& fruits) {
+        int n = fruits.size();
+        int i = 0;
+        int j = 0;
+        unordered_map<int, int> mp;
+        int maxL = 0;
+        while(j<n){
+            mp[fruits[j]]++;
+            if(mp.size()>2){
+                mp[fruits[i]]--;
+                if(mp[fruits[i]] == 0) mp.erase(fruits[i]);
+                i++;
+            }else{
+                maxL = max(maxL, j-i+1);
+            }
+            j++;
+        }
+        return maxL;
+    }
+};
