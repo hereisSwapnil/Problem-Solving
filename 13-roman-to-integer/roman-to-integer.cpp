c37cbff0038765e108c16072d08bcd39878ce4f1
Swapnil Singh <85278243+hereisSwapnil@users.noreply.github.com>
2025-09-01 12:18:21 +0530

    Time: 10 ms (30.62%) | Memory: 13.5 MB (19.11%) - LeetSync

---
+++ 13-roman-to-integer/roman-to-integer.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    int romanToInt(string s) {
+        unordered_map<char, int> mp = {
+            {'I', 1},
+            {'V', 5},
+            {'X', 10},
+            {'L', 50},
+            {'C', 100},
+            {'D', 500},
+            {'M', 1000}
+        };
+        int num = 0;
+        int i = 0;
+        while(i<s.length()){
+            if(s[i] == 'I' && s[i+1] == 'V'){
+                num += 4;
+                i+=2;
+            }else if(s[i] == 'I' && s[i+1] == 'X'){
+                num += 9;
+                i+=2;
+            }else if(s[i] == 'X' && s[i+1] == 'L'){
+                num += 40;
+                i+=2;
+            }else if(s[i] == 'X' && s[i+1] == 'C'){
+                num += 90;
+                i+=2;
+            }else if(s[i] == 'C' && s[i+1] == 'D'){
+                num += 400;
+                i+=2;
+            }else if(s[i] == 'C' && s[i+1] == 'M'){
+                num += 900;
+                i+=2;
+            }else{
+                num += mp[s[i]];
+                i++;
+            }
+        }
+        return num;
+    }
+};
