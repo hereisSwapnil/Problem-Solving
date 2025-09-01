997aaabe4930ef358fc5d874c7419b3f36e63b61
Swapnil Singh <85278243+hereisSwapnil@users.noreply.github.com>
2025-09-01 15:43:53 +0530

    Time: 15 ms (10.00%) | Memory: 13.5 MB (8.45%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.cpp
+++ 13-roman-to-integer/roman-to-integer.cpp
@@ -13,28 +13,12 @@ public:
         int num = 0;
         int i = 0;
         while(i<s.length()){
-            if(s[i] == 'I' && s[i+1] == 'V'){
-                num += 4;
-                i+=2;
-            }else if(s[i] == 'I' && s[i+1] == 'X'){
-                num += 9;
-                i+=2;
-            }else if(s[i] == 'X' && s[i+1] == 'L'){
-                num += 40;
-                i+=2;
-            }else if(s[i] == 'X' && s[i+1] == 'C'){
-                num += 90;
-                i+=2;
-            }else if(s[i] == 'C' && s[i+1] == 'D'){
-                num += 400;
-                i+=2;
-            }else if(s[i] == 'C' && s[i+1] == 'M'){
-                num += 900;
-                i+=2;
+            if(mp[s[i]]<mp[s[i+1]]){
+                num -= mp[s[i]];
             }else{
                 num += mp[s[i]];
-                i++;
             }
+            i++;
         }
         return num;
     }
