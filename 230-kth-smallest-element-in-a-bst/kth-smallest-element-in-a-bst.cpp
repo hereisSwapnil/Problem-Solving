8b502a5c3ddce32121a9fdaeb07d0991e3acc5bd
Swapnil Singh <85278243+hereisSwapnil@users.noreply.github.com>
2025-09-12 16:49:38 +0530

    Time: 195 ms (3.48%) | Memory: 525.7 MB (7.56%) - LeetSync

--- 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.cpp
+++ 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.cpp
@@ -10,21 +10,19 @@
  * };
  */
 class Solution {
-    int ans = -1, count = 0;
 public:
     // Naive Approach
-    void inorderArr(TreeNode* root, int k){
-        if(!root) return;
-        inorderArr(root->left, k);
-        count++;
-        if(count == k){
-            ans = root->val;
-            return;
-        }
-        inorderArr(root->right, k);
+    vector<int> inorderArr(TreeNode* root, vector<int> &arr){
+        if(!root) return arr;
+        inorderArr(root->left, arr);
+        arr.push_back(root->val);
+        inorderArr(root->right, arr);
+        return arr;
     }
     int kthSmallest(TreeNode* root, int k) {
-        inorderArr(root, k);
-        return ans;
+        vector<int> arr;
+        arr = inorderArr(root, arr);
+        sort(arr.begin(), arr.end());
+        return arr[k-1];
     }
 };
