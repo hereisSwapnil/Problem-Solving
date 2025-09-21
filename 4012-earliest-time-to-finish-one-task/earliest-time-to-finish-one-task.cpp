7c689d87ea31f2f8573094a83317827b5aae5d16
Swapnil Singh <85278243+hereisSwapnil@users.noreply.github.com>
2025-09-21 07:26:03 +0530

    Time: 0 ms (100.00%) | Memory: 36.7 MB (78.54%) - LeetSync

--- 4012-earliest-time-to-finish-one-task/earliest-time-to-finish-one-task.cpp
+++ 4012-earliest-time-to-finish-one-task/earliest-time-to-finish-one-task.cpp
@@ -2,8 +2,9 @@ class Solution {
 public:
     int earliestTime(vector<vector<int>>& tasks) {
         int less = INT_MAX;
-        for(auto i: tasks){
-            int sum = i[0]+i[1];
+        int n = tasks.size();
+        for(int i = 0; i<n; i++){
+            int sum = tasks[i][0]+tasks[i][1];
             if(sum<less){
                 less = sum;
             }
