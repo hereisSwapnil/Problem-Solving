b1322db6fc622fa41308761933b786a94d6e8743
Swapnil Singh <85278243+hereisSwapnil@users.noreply.github.com>
2025-10-05 00:16:06 +0530

    Time: 0 ms (100.00%) | Memory: 7.6 MB (86.71%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,17 +1,28 @@
 class Solution {
 public:
-    vector<int> dp;
     // Using Dynamic Programming DP
-    int rec(int n){
-        if(n <= 1) return n;
+    // vector<int> dp;
+    // int rec(int n){
+    //     if(n <= 1) return n;
 
-        if(dp[n] != -1) return dp[n];
-        else{
-            return dp[n] = rec(n-1) + rec(n-2);
-        }
-    }
+    //     if(dp[n] != -1) return dp[n];
+    //     else{
+    //         return dp[n] = rec(n-1) + rec(n-2);
+    //     }
+    // }
+    // int fib(int n) {
+    //     dp.assign(n+1, -1);
+    //     return rec(n);
+    // }
     int fib(int n) {
-        dp.assign(n+1, -1);
-        return rec(n);
+        if(n<=1) return n;
+        int prevv = 0;
+        int prev = 1;
+        for(int i = 2; i<=n; i++){
+            int temp = prev+prevv;
+            prevv = prev;
+            prev = temp;
+        }
+        return prev;
     }
 };
